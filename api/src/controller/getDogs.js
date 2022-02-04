fce7fd967d0c12e738af3db4564253b8f245a0a4
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-02-04 12:04:51 -0300

    get a dog by breed name and get all dogs

--- api/src/controller/getDogs.js
+++ api/src/controller/getDogs.js
@@ -1,31 +1,13 @@
-const axios = require('axios');
-const { Dog, Temperament } = require('../db');
 const getAllDogs = require('./getAllDogs');
-const URL = `https://api.thedogapi.com/v1/breeds`
 
 const getDogs = async (req, res) => {
   const { name } = req.query;
   const allDogs = await getAllDogs();
   if (name) {
-    // const dog = [];
     try {
       const dogByName = allDogs.filter(d => d.name.toLowerCase() === name.toLowerCase());
       if (dogByName.length !== 0) return res.json(dogByName);
       else return res.status(404).send('La raza que estas buscando no existe');
-      // const dogDB = await Dog.findAll({
-      //   where: { name: name },
-      //   include: { model: Temperament }
-      // });
-
-      // const dogToPush = dogDB.map(d => {})
-
-      // const dogsApi = await axios.get(`${URL}/search?q=${name}`);
-      // const dogsToFilter = mapDogs(dogsApi.data);
-      // const dogByName = dogsToFilter.filter(d => d.name.toLowerCase() === name.toLowerCase());
-      // console.log('dogByName', dogByName.length);
-      // if (dogByName.length !== 0) return res.json(dogByName);
-      // return res.status(404).send('La raza que estas buscando no existe');
-
     } catch {
       return res.status(404).send('Algo malió sal');
     }
@@ -36,28 +18,6 @@ const getDogs = async (req, res) => {
       return res.status(404).send('Hubo un error');
     }
   }
-  // try {
-  //   const { data } = await axios.get(URL);
-  //   const dogsApi = mapDogs(data);
-  //   return res.json(dogsApi);
-  // } catch (error) {
-  //   return res.status(404).send(error);
-  // }
 };
-
-// function mapDogs(array) {
-//   const dogsArr = array.map(d => {
-//     return {
-//       id: d.id,
-//       name: d.name,
-//       height: d.height.metric,
-//       weight: d.weight.metric,
-//       life_span: d.life_span,
-//       ...(d.temperament ? { temperament: d.temperament } : null),
-//       ...(d.image ? { image: d.image.url } : null)
-//     }
-//   });
-//   return dogsArr;
-// };
 
 module.exports = getDogs;
