c57320cfd52a906e0ad123a088920f802b63d4e4
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-02-04 15:41:08 -0300

    working routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,37 +2,15 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const getDogs = require('../controller/getDogs');
+const getDogsById = require('../controller/getDogsById');
+const getTemperaments = require('../controller/getTemperaments');
+const postDog = require('../controller/postDog');
+
 const router = Router();
 
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-// const getApiInfo = async () => {
-//   const apiUrl = await axios.get(`https://api.thedogapi.com/v1/breeds?api_key=${YOUR_API_KEY}&limit=10`);
-//   const apiInfo = await apiUrl.data.map( e => {
-//     return {
-//       name: e.name,
-//       height: e.height.metric,
-//       weight: e.weight.metric,
-//       life_span: e.life_span,
-//       temperament: e.temperament
-//     }
-//   });
-//   return apiInfo;
-// };
-
-// const getDbInfo = async () => {
-//   return await Dog.findAll({
-
-//   });
-// };
-
 router.get('/dogs', getDogs);
-
-router.get('/dogs/:idRaza',);
-
-router.get('/temperament',);
-
-router.post('/dog',);
-
+router.get('/dogs/:idRaza', getDogsById);
+router.get('/temperament', getTemperaments);
+router.post('/dog', postDog);
 
 module.exports = router;
