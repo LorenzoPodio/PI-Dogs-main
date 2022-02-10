aaa9531cfe9e0ae75a6b5f4c8b37cc3d24f1a04f
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-02-10 02:01:36 -0300

    index & ruteado

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
+import { NavBar } from './components/NavBar/NavBar';
+import { LandingPage } from './components/LandingPage/LandingPage';
+import { Dogs } from './components/Dogs/Dogs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Dogs</h1>
+      <NavBar/>
+      <Routes>
+        <Route exact path='/' element={<LandingPage/>}/>
+        <Route path='/home'element={<Dogs/>}/>
+      </Routes>
     </div>
   );
 }
