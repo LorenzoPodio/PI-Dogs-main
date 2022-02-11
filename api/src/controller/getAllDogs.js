e2bfe091d0a67f8daa45e396d058dd63a994d25d
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-02-10 21:59:27 -0300

    fixed db temps problem

--- api/src/controller/getAllDogs.js
+++ api/src/controller/getAllDogs.js
@@ -12,27 +12,47 @@ const getApiDogs = async () => {
       height: d.height.metric,
       weight: d.weight.metric,
       life_span: d.life_span,
-      ...(d.temperament ? { temperament: d.temperament } : null),
-      ...(d.image ? { image: d.image.url } : null)
+      ...(d.temperament ? { temperament: d.temperament } : { temperament: 'Default' }),
+      ...(d.image ? { image: d.image.url } : { image: 'https://icon-library.com/images/dog-icon-vector/dog-icon-vector-4.jpg' })
     }
   });
   return dataToSend;
 };
 
 const getDbDogs = async () => {
-  return await Dog.findAll({
+  const arryTempToString = (temperament) =>{
+    return temperament.map(t => t.name).join(', ');
+  };
+
+  const dogsDB = await Dog.findAll({
     include: {
       model: Temperament,
       attributes: ['name'],
       through: { attributes: [] }
     }
   });
+
+  const mapedDogs = dogsDB.map(d => {
+    console.log('d', d)
+    return {
+      id: d.id,
+      name: d.name,
+      height: d.height,
+      weight: d.weight,
+      life_span: d.life_span,
+      temperament: arryTempToString(d.temperaments),
+      image: 'https://icon-library.com/images/dog-icon-vector/dog-icon-vector-4.jpg'
+    }
+  });
+
+  return mapedDogs;
 };
 
 const getAllDogs = async () => {
   const apiDogs = await getApiDogs();
   const dbDogs = await getDbDogs();
   const allDogs = apiDogs.concat(dbDogs);
+  console.log('allDogs.length', allDogs.length)
   return allDogs;
 };
 
