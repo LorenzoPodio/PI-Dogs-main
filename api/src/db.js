8ceceade7e0f9976a2e42135fae69a031ca369c3
Lorenzo Podio <lorenzopedropodio@gmail.com>
2022-06-07 22:03:03 -0300

    deploy config

--- api/src/db.js
+++ api/src/db.js
@@ -2,16 +2,35 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 console.log('DB_USER', DB_USER);
 // console.log('YOUR_API_KEY', YOUR_API_KEY);
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = process.env.NODE_ENV === 'production' ?
+  new Sequelize({
+    database: DB_NAME,
+    dialect: 'postgres',
+    host: DB_HOST,
+    port: 5432,
+    username: DB_USER,
+    password: DB_PASSWORD,
+    pool: {
+      max: 3,
+      min: 1,
+      idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+      keepAlive: true,
+    },
+    ssl: true 
+  })
+  : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  });
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
@@ -36,8 +55,8 @@ const { Dog, Temperament } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
-Temperament.belongsToMany(Dog, {through: 'dog_temperament'});
-Dog.belongsToMany(Temperament, {through: 'dog_temperament'});
+Temperament.belongsToMany(Dog, { through: 'dog_temperament' });
+Dog.belongsToMany(Temperament, { through: 'dog_temperament' });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import './index.css';
 import App from './App';
+import axios from 'axios';
 import { Provider } from "react-redux";
 import { store } from './redux/store';
 import { BrowserRouter as Router } from "react-router-dom";
+import dotenv from 'dotenv';
+dotenv.config();
+
+axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 
 ReactDOM.render(
   <Provider store={store}>
